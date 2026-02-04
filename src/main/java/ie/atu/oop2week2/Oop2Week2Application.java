81484e1858d966d5c3f5f10be26b0cd86df5523a
LiamPlower-STUDENT <G00467316@atu.ie>
2026-02-04 16:19:01 +0000

    Setup Project

---
+++ src/main/java/ie/atu/oop2week2/Oop2Week2Application.java
@@ -0,0 +1,13 @@
+package ie.atu.oop2week2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oop2Week2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Oop2Week2Application.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/oop2week2/Oop2Week2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.oop2week2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oop2Week2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
